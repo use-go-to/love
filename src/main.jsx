ab8f146239c7be804ab71cd34ef385a69c9726de
use-go-to <54232215+use-go-to@users.noreply.github.com>
2026-04-13 22:40:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// 🔥 FIX PWA iOS : reset service worker + cache
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    for (const r of regs) {
+      r.unregister()
+    }
+  })
+
+  caches.keys().then(keys => {
+    for (const k of keys) {
+      caches.delete(k)
+    }
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
